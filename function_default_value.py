ca3c96832ffae5e93981282a79cb1c9873c7d800
hp <developers@instaspiel.com>
2018-08-05 18:09:23 +0530

    function defauly program

---
+++ function_default_value.py
@@ -0,0 +1,34 @@
+def my_function(country = "India"):
+  print("I am from " + country)
+
+
+
+
+
+
+
+
+
+my_function("Sweden")
+
+my_function("India")
+
+my_function()           #default
+
+my_function("Brazil")
+
+
+
+
+'''
+Result
+
+********************
+
+I am from Sweden
+I am from India
+I am from India
+I am from Brazil
+
+************************
+'''
