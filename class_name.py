6d85c3a15df8773c289f632f022c56a56c6ef4d7
Vinay <vinaypatidar12345@gmail.com>
2018-08-06 02:42:34 +0530

    class simple program

---
+++ class_name.py
@@ -0,0 +1,28 @@
+class Person:
+     def __init__(self, name, age):
+        self.n = name
+        self.a = age
+
+
+
+
+
+
+ab = Person("Abhishek",24)
+vin = Person("Vinay",22)
+pwn = Person("Pawan",25)
+
+daji = Person("Ramchndra bhagat",50)
+
+print(ab.n)
+print(ab.a)
+
+
+
+print(daji.n)
+print(daji.a)
+
+
+print("frgsdfgdf"+vin.n)
+
+print("Vinay Age:--->   "+str(vin.a))
