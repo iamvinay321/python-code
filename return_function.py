76f9cd8569a30a80f522ebb666f2e0c61209c91d
hp <developers@instaspiel.com>
2018-08-06 01:40:48 +0530

    return program and conmsole input program print series

---
+++ return_function.py
@@ -0,0 +1,16 @@
+def my_function(x):
+  return 5 * x
+a = my_function(12)
+print(a)
+
+
+
+print("--------------------------------")
+
+def series(s):
+   for x in range(s,100):
+      print(x)
+
+
+print("Enter value you will start series")   
+series(input())         # and   s= input()   series(s)
